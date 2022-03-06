a1c0a589c9eac5c1a66e67c2a3e6a381ef4aa4db
Rony <rony.msalu@gmail.com>
2022-03-05 23:38:17 -0300

    Mentoria e curso

---
+++ src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,45 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descriacao;
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescriacao() {
+        return descriacao;
+    }
+
+    public void setDescriacao(String descriacao) {
+        this.descriacao = descriacao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descriacao='" + descriacao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
